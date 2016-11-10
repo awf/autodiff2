d5eee36475e845cb989e992eda5dde991cf8d975
amirsh <amir.shaikhha@epfl.ch>
2016-11-10 13:08:50 +0100

    Updated memory usage script

--- fsmooth/run.sh
+++ fsmooth/run.sh
@@ -1,7 +1,17 @@
 declare -a files=("ba.exe" "ba_bump.exe" "ba_opt.exe" "ba_opt_bump.exe" "ba_dps.exe" "ba_dps_bump.exe" "ba_opt_dps.exe" "ba_opt_dps_bump.exe")
 
+echo "=== Run time Usage ==="
+
 for i in "${files[@]}"
 do
    echo "$i"
    eval "./$i"
+done
+
+echo "=== Memory Usage ==="
+
+for i in "${files[@]}"
+do
+   echo "$i"
+   eval "./memusg.sh ./$i"
 done
