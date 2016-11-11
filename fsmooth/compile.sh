85d87789c16439af142b75fef7d333a18876cfa4
amirsh <amir.shaikhha@epfl.ch>
2016-11-11 15:55:41 +0100

    Updated benchmarking scripts

--- fsmooth/compile.sh
+++ fsmooth/compile.sh
@@ -1,3 +1,4 @@
+echo "Compiling BA ..."
 clang ba.c -O3 -o ba.exe
 # clang ba.c -O3 -DBUMP -o ba_bump.exe
 clang ba.c -O3 -DFUSED -o ba_opt.exe
@@ -6,8 +7,13 @@ clang ba.c -O3 -DDPS -o ba_dps.exe
 # clang ba.c -O3 -DBUMP -DDPS -o ba_dps_bump.exe
 clang ba.c -O3 -DDPS -DFUSED -o ba_opt_dps.exe
 clang ba.c -O3 -DBUMP -DDPS -DFUSED -o ba_opt_dps_bump.exe
+echo "Compiling GMM ..."
 clang gmm.c -O3 -o gmm.exe
 clang gmm.c -O3 -DFUSED -o gmm_opt.exe
 clang gmm.c -O3 -DDPS -o gmm_dps.exe
 clang gmm.c -O3 -DDPS -DFUSED -o gmm_opt_dps.exe
 clang gmm.c -O3 -DBUMP -DDPS -DFUSED -o gmm_opt_dps_bump.exe
+echo "Compiling CPP files ..."
+cd ..; cd benchmark_gmm; make
+cd ..; cd benchmark_ba; make
+cd ..; cd fsmooth
--- fsmooth/run.sh
+++ fsmooth/run.sh
@@ -1,5 +1,6 @@
-# declare -a files=("ba.exe" "ba_opt.exe" "ba_dps.exe" "ba_opt_dps.exe" "ba_opt_dps_bump.exe")
-declare -a files=("gmm.exe" "gmm_opt.exe" "gmm_dps.exe" "gmm_opt_dps.exe" "gmm_opt_dps_bump.exe")
+declare -a files=("ba.exe" "ba_opt.exe" "ba_dps.exe" "ba_opt_dps.exe" "ba_opt_dps_bump.exe" "../benchmark_ba/ba_cpp.out")
+# declare -a files=("gmm.exe" "gmm_opt.exe" "gmm_dps.exe" "gmm_opt_dps.exe" "gmm_opt_dps_bump.exe" 
+# 	"../benchmark_gmm/gmm_cpp.out" "../benchmark_gmm/gmm_fixed_cpp.out")
 
 echo "=== Run time Usage ==="
 
