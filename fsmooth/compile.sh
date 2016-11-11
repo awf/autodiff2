eaf4c98f49e4cf6408a6719b53ed6b1eeed0ff47
amirsh <amir.shaikhha@epfl.ch>
2016-11-11 15:45:43 +0100

    Removed unnecessary benchmarks

--- fsmooth/compile.sh
+++ fsmooth/compile.sh
@@ -1,12 +1,13 @@
 clang ba.c -O3 -o ba.exe
-clang ba.c -O3 -DBUMP -o ba_bump.exe
+# clang ba.c -O3 -DBUMP -o ba_bump.exe
 clang ba.c -O3 -DFUSED -o ba_opt.exe
-clang ba.c -O3 -DBUMP -DFUSED -o ba_opt_bump.exe
+# clang ba.c -O3 -DBUMP -DFUSED -o ba_opt_bump.exe
 clang ba.c -O3 -DDPS -o ba_dps.exe
-clang ba.c -O3 -DBUMP -DDPS -o ba_dps_bump.exe
+# clang ba.c -O3 -DBUMP -DDPS -o ba_dps_bump.exe
 clang ba.c -O3 -DDPS -DFUSED -o ba_opt_dps.exe
 clang ba.c -O3 -DBUMP -DDPS -DFUSED -o ba_opt_dps_bump.exe
 clang gmm.c -O3 -o gmm.exe
 clang gmm.c -O3 -DFUSED -o gmm_opt.exe
 clang gmm.c -O3 -DDPS -o gmm_dps.exe
 clang gmm.c -O3 -DDPS -DFUSED -o gmm_opt_dps.exe
+clang gmm.c -O3 -DBUMP -DDPS -DFUSED -o gmm_opt_dps_bump.exe
