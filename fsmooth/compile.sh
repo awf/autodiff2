130a78fa7be45f09aff4bb4d6424cfb2cb6eb0e3
amirsh <amir.shaikhha@epfl.ch>
2017-02-26 21:50:19 +0100

    compilation and run scripts updated for new experimentation config

--- fsmooth/compile.sh
+++ fsmooth/compile.sh
@@ -9,23 +9,28 @@ clang ba.c -O3 -DDPS -DFUSED -DHOIST -o ba_opt_dps_hoist.exe
 clang ba.c -O3 -DBUMP -DDPS -o ba_dps_bump.exe
 clang ba.c -O3 -DBUMP -DDPS -DFUSED -o ba_opt_dps_bump.exe
 clang ba.c -O3 -DBUMP -DDPS -DHOIST -DFUSED -o ba_opt_dps_hoist_bump.exe
-# echo "Compiling GMM ..."
-# clang gmm.c -O3 -o gmm.exe
-# clang gmm.c -O3 -DFUSED -o gmm_opt.exe
-# clang gmm.c -O3 -DDPS -o gmm_dps.exe
-# clang gmm.c -O3 -DDPS -DFUSED -o gmm_opt_dps.exe
-# clang gmm.c -O3 -DBUMP -DDPS -DFUSED -o gmm_opt_dps_bump.exe
-# echo "Compiling HT ..."
-# clang ht.c -O3 -o ht.exe
-# clang ht.c -O3 -DGC -lgc -o ht_gc.exe
-# clang ht.c -O3 -DFUSED -o ht_opt.exe
-# clang ht.c -O3 -DDPS -o ht_dps.exe
-# clang ht.c -O3 -DDPS -DHOIST -o ht_dps_hoist.exe
-# clang ht.c -O3 -DDPS -DFUSED -o ht_opt_dps.exe
-# clang ht.c -O3 -DDPS -DFUSED -DHOIST -o ht_opt_dps_hoist.exe
-# clang ht.c -O3 -DBUMP -DDPS -o ht_dps_bump.exe
-# clang ht.c -O3 -DBUMP -DDPS -DFUSED -o ht_opt_dps_bump.exe
-# clang ht.c -O3 -DBUMP -DDPS -DHOIST -DFUSED -o ht_opt_dps_hoist_bump.exe
+echo "Compiling GMM ..."
+clang gmm.c -O3 -o gmm.exe
+clang gmm.c -O3 -DGC -lgc -o gmm_gc.exe
+clang gmm.c -O3 -DFUSED -o gmm_opt.exe
+clang gmm.c -O3 -DDPS -o gmm_dps.exe
+clang gmm.c -O3 -DDPS -DHOIST -o gmm_dps_hoist.exe
+clang gmm.c -O3 -DDPS -DFUSED -o gmm_opt_dps.exe
+clang gmm.c -O3 -DDPS -DFUSED -DHOIST -o gmm_opt_dps_hoist.exe
+clang gmm.c -O3 -DBUMP -DDPS -o gmm_dps_bump.exe
+clang gmm.c -O3 -DBUMP -DDPS -DFUSED -o gmm_opt_dps_bump.exe
+clang gmm.c -O3 -DBUMP -DDPS -DHOIST -DFUSED -o gmm_opt_dps_hoist_bump.exe
+echo "Compiling HT ..."
+clang ht.c -O3 -o ht.exe
+clang ht.c -O3 -DGC -lgc -o ht_gc.exe
+clang ht.c -O3 -DFUSED -o ht_opt.exe
+clang ht.c -O3 -DDPS -o ht_dps.exe
+clang ht.c -O3 -DDPS -DHOIST -o ht_dps_hoist.exe
+clang ht.c -O3 -DDPS -DFUSED -o ht_opt_dps.exe
+clang ht.c -O3 -DDPS -DFUSED -DHOIST -o ht_opt_dps_hoist.exe
+clang ht.c -O3 -DBUMP -DDPS -o ht_dps_bump.exe
+clang ht.c -O3 -DBUMP -DDPS -DFUSED -o ht_opt_dps_bump.exe
+clang ht.c -O3 -DBUMP -DDPS -DHOIST -DFUSED -o ht_opt_dps_hoist_bump.exe
 # echo "Compiling Micro Benchmarks ..."
 # echo "Compiling Add3 ..."
 # clang micro.c -O3 -DADD3 -o micro_add3.exe
@@ -45,9 +50,9 @@ clang ba.c -O3 -DBUMP -DDPS -DHOIST -DFUSED -o ba_opt_dps_hoist_bump.exe
 # clang micro.c -O3 -DDOT -DDPS -o micro_dot_dps.exe
 # clang micro.c -O3 -DDOT -DDPS -DFUSED -o micro_dot_opt_dps.exe
 # clang micro.c -O3 -DDOT -DBUMP -DDPS -DFUSED -o micro_dot_opt_dps_bump.exe
-# echo "Compiling CPP files ..."
-# cd ..; cd benchmark_gmm; make
-# cd ..; cd benchmark_ba; make
+echo "Compiling CPP files ..."
+cd ..; cd benchmark_gmm; make
+cd ..; cd benchmark_ba; make
 cd ..; cd benchmark_ht; make
 # cd ..; cd benchmark_micro; make
-# cd ..; cd fsmooth
+cd ..; cd fsmooth
--- fsmooth/run.sh
+++ fsmooth/run.sh
@@ -1,12 +1,14 @@
 declare -a files=(
-	# "ba.exe" "ba_opt.exe" "ba_dps.exe" "ba_opt_dps.exe" "ba_opt_dps_bump.exe" 
-	# "../benchmark_ba/ba_cpp.exe" "../benchmark_ba/ba_eigen_cpp.exe"
+	# # "ba.exe" "ba_opt.exe" "ba_dps.exe" "ba_opt_dps.exe" "ba_opt_dps_bump.exe" 
+	"ba.exe" "ba_gc.exe" "ba_opt.exe" "ba_dps.exe" "ba_opt_dps.exe" "ba_opt_dps_hoist.exe" "ba_opt_dps_hoist_bump.exe"
+	"../benchmark_ba/ba_cpp.exe" "../benchmark_ba/ba_eigen_cpp.exe"
 
- #    "gmm.exe" "gmm_opt.exe" "gmm_dps.exe" "gmm_opt_dps.exe" "gmm_opt_dps_bump.exe" 
-	# "../benchmark_gmm/gmm_fixed_cpp.exe" "../benchmark_gmm/gmm_eigen_cpp.exe"
+	# # "gmm.exe" "gmm_opt.exe" "gmm_dps.exe" "gmm_opt_dps.exe" "gmm_opt_dps_bump.exe" 
+	"gmm.exe" "gmm_gc.exe" "gmm_opt.exe" "gmm_dps.exe" "gmm_opt_dps.exe" "gmm_opt_dps_hoist.exe" "gmm_opt_dps_hoist_bump.exe"
+	"../benchmark_gmm/gmm_fixed_cpp.exe" "../benchmark_gmm/gmm_eigen_cpp.exe"
 
-	# "ht.exe" "ht_opt.exe" "ht_dps.exe" "ht_opt_dps.exe" "ht_opt_dps_bump.exe" 
-	"ht.exe" "ht_gc.exe" "ht_opt.exe" "ht_dps.exe" "ht_dps_hoist.exe" "ht_opt_dps.exe" "ht_opt_dps_hoist.exe" "ht_dps_bump.exe" "ht_opt_dps_bump.exe" "ht_opt_dps_hoist_bump.exe"
+	# # "ht.exe" "ht_opt.exe" "ht_dps.exe" "ht_opt_dps.exe" "ht_opt_dps_bump.exe" 
+	"ht.exe" "ht_gc.exe" "ht_opt.exe" "ht_dps.exe" "ht_opt_dps.exe" "ht_opt_dps_hoist.exe" "ht_opt_dps_hoist_bump.exe"
 	"../benchmark_ht/ht_cpp.exe"
 	)
 
