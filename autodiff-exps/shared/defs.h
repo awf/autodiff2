719874a7c6a210b2de21a1f94104d160102c632e
amirsh <amir.shaikhha@epfl.ch>
2017-07-25 20:23:09 +0200

    dot product micro benchmark added.

--- autodiff-exps/shared/defs.h
+++ autodiff-exps/shared/defs.h
@@ -26,4 +26,12 @@ typedef struct
 //# Flexion, Abduction, Twist = 'xzy'
 #define HAND_XYZ_TO_ROTATIONAL_PARAMETERIZATION {0, 2, 1} 
 
+template<typename T>
+T dot(int n, const T* const x, const T* const y)
+{
+  T res = 0;
+  for (int i = 0; i < n; i++)
+    res = res + x[i] * y[i];
+  return res;
+}
 
