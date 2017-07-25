2f0bf6670bddea6b3b57737945d69b2508c4f698
amirsh <amir.shaikhha@epfl.ch>
2017-07-25 23:13:07 +0200

    BA benchmark for Tapanade added.

--- autodiff-exps/tapanade-main.cpp
+++ autodiff-exps/tapanade-main.cpp
@@ -7,6 +7,68 @@
 #include "shared/defs.h"
 
 #if defined DO_BA
+#include "tapanade/ba_d-all.h"
+
+double sqsum(int n, double *x)
+{
+  int i;
+  double res;
+  res = 0;
+  for (i = 0; i < n; i++)
+    res = res + x[i] * x[i];
+  return res;
+}
+
+
+int test_ba()
+{
+    // auto cam = vec(
+    //     0.1, 0.1, 0.1,     // ROT_IDX = 0;    
+    //     0.2,0.1,0.3,       // CENTER_IDX = 3;
+    //     1.2,               // FOCAL_IDX = 6;
+    //     0.01, 0.03,        // X0_IDX = 7;
+    //     0.009, 1.2e-4      // RAD_IDX = 9;
+    //     );
+
+    // Vec3<Real> X = vec(0.03, 0.11, -0.7);
+    double cam[] = { 0.1, 0.1, 0.1,     // ROT_IDX = 0;    
+        0.2,0.1,0.3,       // CENTER_IDX = 3;
+        1.2,               // FOCAL_IDX = 6;
+        0.01, 0.03,        // X0_IDX = 7;
+        0.009, 1.2e-4      // RAD_IDX = 9;
+        };
+    double X[] = {0.03, 0.11, -0.7};
+
+    // boost::timer::auto_cpu_timer t;
+    timer_t t = tic();
+
+    // Debug 150s 
+    // Release 1s
+    double total = 0;
+    int N = 10000000;
+#ifdef _DEBUG
+    N = N / 100;  // Debug is roughly this much slower than release -- multiply timings.
+#endif
+    for (int count = 0; count < N; ++count) {
+        X[0] = 1.0 / (2.0 + count);
+        cam[5] = 1.0 + count * 1e-6;
+
+        double proj[2];
+        double proj_d[2];
+        project_d(cam, cam , X, X, proj, proj_d);
+
+        total += sqsum(2, proj_d);
+    }
+
+    // std::cout << "total =" << total << ", time per call = " << t.elapsed().wall/double(N) << "ns" << std::endl;
+    // std::cout << "total =" << total << std::endl;
+    auto elapsed = toc(t);
+    printf("total =%f, time per call = %f ms\n", total, elapsed / double(N));
+    
+    return 0;
+}
+
+
 #elif defined DO_MICRO
 
 Real dist(int seed) {
@@ -70,6 +132,7 @@ void test_micro(cardinality_t N)
 int main(int argc, char *argv[])
 {
 #if defined DO_BA
+  test_ba();
 #elif defined DO_MICRO
   if(argc != 2) {
     printf("You should use the following format for running this program: %s <Number of Iterations>\n", argv[0]);
