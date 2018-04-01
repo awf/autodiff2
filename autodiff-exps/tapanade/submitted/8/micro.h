e7676129da2cc3713f7798085594c378f5eeaaf6
amirsh <amir.shaikhha@epfl.ch>
2018-04-01 20:00:48 +0200

    Added Tapenade generated reverse and forward for 3 micro benchmarks.

---
+++ autodiff-exps/tapanade/submitted/8/micro.h
@@ -0,0 +1,23 @@
+double* vec_add(int n, double* x, double* y) {
+  double* res = malloc(n * sizeof(double));
+  for(int i = 0; i< n; i++) {
+  	res[i] = x[i] + y[i];
+  }
+  return res;
+}
+
+double* vec_elem_mult(int n, double* x, double* y) {
+  double* res = malloc(n * sizeof(double));
+  for(int i = 0; i< n; i++) {
+  	res[i] = x[i] * y[i];
+  }
+  return res;
+}
+
+double* vec_scal_mult(int n, double* x, double y) {
+  double* res = malloc(n * sizeof(double));
+  for(int i = 0; i< n; i++) {
+  	res[i] = x[i] * y * y;
+  }
+  return res;
+}
