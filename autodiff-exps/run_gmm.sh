6144eaa1df6d82a6f98a69d3defb0a94860091eb
amirsh <amir.shaikhha@epfl.ch>
2018-04-03 14:17:51 +0200

    New results for GMM.

--- autodiff-exps/run_gmm.sh
+++ autodiff-exps/run_gmm.sh
@@ -6,8 +6,11 @@ declare -a files=(
 	)
 
 declare -a params=(
-	"gmm_d2_K5" "gmm_d2_K10" "gmm_d2_K25" "gmm_d2_K50"
-	"gmm_d10_K5" "gmm_d10_K10" "gmm_d10_K25" "gmm_d10_K50"
+	"gmm_d2_K5" "gmm_d2_K10" "gmm_d2_K25" "gmm_d2_K50" "gmm_d2_K100" "gmm_d2_K200"
+	"gmm_d10_K5" "gmm_d10_K10" "gmm_d10_K25" "gmm_d10_K50" "gmm_d10_K100" "gmm_d10_K200"
+	"gmm_d20_K5" "gmm_d20_K10" "gmm_d20_K25" "gmm_d20_K50" "gmm_d20_K100" "gmm_d20_K200"
+	"gmm_d32_K5" "gmm_d32_K10" "gmm_d32_K25" "gmm_d32_K50" "gmm_d32_K100" "gmm_d32_K200"
+	"gmm_d64_K5" "gmm_d64_K10" "gmm_d64_K25" "gmm_d64_K50" "gmm_d64_K100" "gmm_d64_K200"
 	)
 
 for p in "${params[@]}"
@@ -16,6 +19,6 @@ do
 	for f in "${files[@]}"
 	do
 		echo "$f"
-		eval "$f ../gmm_instances/10k/ ../results_ad/ $p 100 5"
+		eval "gtimeout 600 $f ../gmm_instances/10k/ ../results_ad/ $p 5 5"
 	done
 done
