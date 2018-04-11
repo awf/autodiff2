501ac9ec901ac930c29be3a620393405f2ea7e19
Amir Shaikhha <amir.shaikhha@epfl.ch>
2018-04-11 06:20:20 +0200

    Results for ba_rod.

--- autodiff-exps/micro/run.sh
+++ autodiff-exps/micro/run.sh
@@ -3,14 +3,16 @@ declare -a files=(
 	# "./add_tap_rev.exe" "./add_tap_for.exe" "./add_diff.exe" "./add_diff_dps.exe" 
 	# "./dot_tap_rev.exe" "./dot_tap_for.exe" "./dot_diff.exe" "./dot_diff_dps.exe"
 	# "./max_tap_rev.exe" "./max_tap_for.exe" "./max_diff.exe" "./max_diff_dps.exe"
-	"./lse_tap_rev.exe" "./lse_tap_for.exe" "./lse_tap_fused_rev.exe" "./lse_tap_fused_for.exe" 
-	"./lse_diff.exe" "./lse_diff_dps.exe"
+	# "./lse_tap_rev.exe" "./lse_tap_for.exe" "./lse_tap_fused_rev.exe" "./lse_tap_fused_for.exe" 
+	# "./lse_diff.exe" "./lse_diff_dps.exe"
+	"./ba_rod_tap_rev.exe" "./ba_rod_tap_for.exe" "./ba_rod_diff_fused.exe"
 	)
 
 declare -a params=(
-	"800" "1600" "3200" "6400" "12800" "25600" "51200"  # MULTS & DOT & MAX & LSE
+	# "800" "1600" "3200" "6400" "12800" "25600" "51200"  # MULTS & DOT & MAX & LSE
 	# "200" "400" "800" "1600" "3200" "6400" "12800" # ADD
-	"102400" "204800" "409600" "819200" "1638400"
+	# "102400" "204800" "409600" "819200" "1638400" # nothing
+	"86" "161" "311" "611" "1211" "2411" "4811" "9611" # BA_ROD
 	)
 
 for p in "${params[@]}"
--- autodiff-exps/plotting/README.md
+++ autodiff-exps/plotting/README.md
@@ -12,4 +12,5 @@ scala ExtractCSV.scala ../../results_ad/micro/mults_1.txt
 scala ExtractCSV.scala ../../results_ad/micro/dot_1.txt 
 scala ExtractCSV.scala ../../results_ad/micro/max_1.txt 
 scala ExtractCSV.scala ../../results_ad/micro/lse_1.txt 
+scala ExtractCSV.scala ../../results_ad/ba/ba_rod_1.txt 
 ```
